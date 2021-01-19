9b035b28127753c1fcb08b622276ebff4050b541
MohammadSaffaf <mhmdsaffha@gmail.com>
2021-01-19 14:45:54 +0100

    lev02

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
-
-// const ShopItem = require('./models/designshop')
-
-
+const fetch = require('node-fetch');
+const mongoose = require('mongoose')
+mongoose.connect(process.env.dbUri,{useNewUrlParser: true, useUnifiedTopology: true})
+.then(result =>{
 app.listen(process.env.PORT,()=> console.log(`http://localhost:${process.env.PORT}`))
+})
+.catch(err => console.log(err))
 
 
 app.use(express.static('public'))
@@ -14,6 +15,18 @@ app.set('view engine','ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+
 app.get('/' , (req, res) =>{
-    res.render('index')
+    let mydata;
+  fetch(process.env.mvUriKay)
+    .then((res) => res.json())
+    .then((data) => {
+        // console.log(data);
+        mydata = data;
+      res.render("index", { mydata : mydata});
+      console.log(mydata);
+    })
+    .catch((err) => console.log(err));
 })
+
+// app.listen(process.env.PORT,()=> console.log(`http://localhost:${process.env.PORT}`))
