19bd82e17f06776757a96b94abf4a8fa8246eecb
MohammadSaffaf <mhmdsaffha@gmail.com>
2021-01-20 13:30:42 +0100

    Search

--- index.js
+++ index.js
@@ -15,18 +15,41 @@ app.set('view engine','ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-
+//Home page____________
 app.get('/' , (req, res) =>{
-    let mydata;
+    
   fetch(process.env.mvUriKay)
-    .then((res) => res.json())
-    .then((data) => {
-        // console.log(data);
-        mydata = data;
-      res.render("index", { mydata : mydata});
-      console.log(mydata);
-    })
-    .catch((err) => console.log(err));
+  .then(res => res.json())
+  .then(json => {
+      // console.log(json)
+      res.render("index", { mydata: json.results  })
+  })
+.catch(err=>console.log(err))
 })
 
-// app.listen(process.env.PORT,()=> console.log(`http://localhost:${process.env.PORT}`))
+//search _____________
+//link zu search https://developers.themoviedb.org/3/search/search-movies
+// nach (en-US&) soll mann (query=${req.body.mySearch}) fügen
+
+app.post('/movie-search' , (req, res) =>{
+    
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.myKey}&language=en-US&query=${req.body.mySearch}&page=1&include_adult=false`)
+  .then(res => res.json())
+  .then(json => {
+      // console.log(json)
+      res.render("index", { mydata: json.results  })
+  })
+.catch(err=>console.log(err))
+})
+
+app.get('/moviesdetails/:id' , (req, res) =>{
+    
+  fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.myKey}&language=en-US`)
+  // .findById(req.params.id)
+  .then(res => res.json())
+  .then(json => {
+      console.log(json)
+      res.render("moviesdetails", { mydata2: json })
+  })
+.catch(err=>console.log(err))
+})
