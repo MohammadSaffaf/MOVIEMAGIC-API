84b924c689c3dec70c1cc296b98f05fa8c471a1f
MohammadSaffaf <mhmdsaffha@gmail.com>
2021-01-21 11:42:44 +0100

    Favurite

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const fetch = require('node-fetch');
 const mongoose = require('mongoose')
+const filmdetails = require("./models/filmdetails");
 mongoose.connect(process.env.dbUri,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(result =>{
 app.listen(process.env.PORT,()=> console.log(`http://localhost:${process.env.PORT}`))
@@ -21,11 +22,14 @@ app.get('/' , (req, res) =>{
   fetch(process.env.mvUriKay)
   .then(res => res.json())
   .then(json => {
-      // console.log(json)
+      // console.log( "test",json)
       res.render("index", { mydata: json.results  })
   })
 .catch(err=>console.log(err))
 })
+
+
+
 
 //search _____________
 //link zu search https://developers.themoviedb.org/3/search/search-movies
@@ -36,7 +40,7 @@ app.post('/movie-search' , (req, res) =>{
   fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.myKey}&language=en-US&query=${req.body.mySearch}&page=1&include_adult=false`)
   .then(res => res.json())
   .then(json => {
-      // console.log(json)
+      console.log(json)
       res.render("index", { mydata: json.results  })
   })
 .catch(err=>console.log(err))
@@ -48,8 +52,54 @@ app.get('/moviesdetails/:id' , (req, res) =>{
   // .findById(req.params.id)
   .then(res => res.json())
   .then(json => {
-      console.log(json)
+      // console.log(json)
       res.render("moviesdetails", { mydata2: json })
   })
 .catch(err=>console.log(err))
+})
+app.get("/myFavuriteList", (req, res) => {
+  filmdetails.find()
+    .then((result) => {
+      res.render("myFavuriteList", { FavItem: result });
+      console.log(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/myFavuriteList/:id' , (req, res) =>{
+    
+  fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.myKey}&language=en-US`)
+  
+  .then(res => res.json())
+  .then(json => {
+    let newFavItem = new filmdetails({
+      backdrop_path: json.backdrop_path,
+      vote_average: json.vote_average,
+      popularity: json.popularity,
+      title: json.title,
+      status: json.status,
+      genres: json.genres,
+      overview: json.overview,
+      release_date: json.release_date,
+      poster_path: json.poster_path,
+    });
+    newFavItem
+      .save()
+      .then((result) => {
+        console.log("new Fav saved");
+        res.redirect("/myFavuriteList");
+      })
+.catch(err=>console.log(err))
+})
+});
+
+
+
+
+app.get('/favuriteDetails/:id', (req, res) => {
+  // console.log(req.params.id);
+  filmdetails.findById(req.params.id)
+  
+  .then(result => res.render('favuriteDetails',{Details: result}))
+  .catch(err => console.log(err))
 })
